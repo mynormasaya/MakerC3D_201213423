a7036f4b5ec24fbbf2902571166f0bdcddf9a084
mynormasaya <mynormasaya@gmail.com>
2017-03-08 23:05:21 -0600

    Hoja de trabajo No. 3 Nombre: Mynor Masaya Carnet: 201213423

--- src/org/compi2/codigo3d/parser/ControlC3D.java
+++ src/org/compi2/codigo3d/parser/ControlC3D.java
@@ -1,5 +1,6 @@
 package org.compi2.codigo3d.parser;
-
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
 /**
  *
  * @author esvux
@@ -8,6 +9,10 @@ public class ControlC3D {
     
     private static int temp = 0;
     private static String c3d = "";
+    private static int lab = 0;
+    private static Stack<String> verdaderas = new Stack<>();
+    private static Stack<String> falsas = new Stack<>();
+    
     
     /**
      * Reinicia las variables estáticas relacionadas con la generación del
@@ -16,6 +21,7 @@ public class ControlC3D {
     public static void reiniciar(){
         temp = 0;
         c3d = "";
+        lab = 0;
     }
     
     /**
@@ -24,6 +30,35 @@ public class ControlC3D {
      */
     public static String generaTemp(){
         return "t$"+temp++;
+    }
+    
+    
+    public static String generaLabelVerdadero(){
+        lab = lab+1;
+        verdaderas.push("L"+lab);
+        return "L"+lab;
+        
+    }
+    
+    public static String generarLabelfinal(){
+        lab = lab+1;
+        
+        return "L"+lab;
+        
+    }
+    
+    public static String generaLabelFalso(){
+        lab = lab+1;
+        falsas.push("L"+lab);
+        return "L"+lab;
+    }
+    
+    public static String popFalso(){
+        return falsas.pop();
+    }
+    
+    public static String popVerdadero(){
+        return verdaderas.pop();
     }
     
     /**
@@ -42,5 +77,66 @@ public class ControlC3D {
     public static String getC3D(){
         return c3d;
     }
+    
+    public static boolean verificarStack(){
+        if(verdaderas.empty() &&falsas.empty()){
+            return true;
+        }else   {
+            return false;
+        }
+    }
+    
+    
+    public static String DevolverTEFALSAS(){
+        String ver="";
+        
+        while(!falsas.empty()){
+            if(ver.equals("")){
+                ver = ver + falsas.pop();
+            }else{
+                ver = ver +","+ falsas.pop();
+            }
+        }
+        ver = ver+":";
+        return ver;
+    }
+    
+    public static String DevolverTEVERDADERAS(){
+        String ver="";
+        
+        while(!verdaderas.empty()){
+           if(ver.equals("")){
+                ver = ver + verdaderas.pop();
+            }else{
+                ver = ver +","+ verdaderas.pop();
+            }
+        }
+        ver = ver+":";
+        return ver;
+    }
+    
+    
+    public static void IntercambioEtiquetas(){
+        Queue<String> aux1 =new LinkedBlockingQueue<>();
+        Queue<String> aux2 = new LinkedBlockingQueue<>();;
+        
+        while(!verdaderas.empty()){
+            aux1.add(verdaderas.pop());
+        }
+        while(!falsas.empty()){
+            aux2.add(falsas.pop());
+        }
+        
+        while(aux1.size()>0){
+            falsas.push(aux1.remove());
+        }
+        
+        while(aux2.size()>0){
+            verdaderas.push(aux2.remove());
+        }
+        
+        
+    }
+    
     
 }
--- src/org/compi2/codigo3d/parser/NodoC3D.java
+++ src/org/compi2/codigo3d/parser/NodoC3D.java
@@ -8,13 +8,27 @@
  */
 public class NodoC3D {
     private String cad;
+    private boolean ver;
 
     public NodoC3D(String cad) {
         this.cad = cad;
+        this.ver = false;
+    }
+    public NodoC3D(String cad,boolean ver) {
+        this.cad = cad;
+        this.ver = ver;
     }
     
     public String getCad(){
         return cad;
+    }
+    
+    public boolean getver(){
+        return ver;
+    }
+    
+    public void setver(boolean ver){
+        this.ver = ver;
     }
         
 }
